073b1a1636ec2122a82891976adb0a8d2cfa74b2
David Rhoderick <david@artlyticalmedia.com>
2020-07-15 16:55:24 +0200

    Allowing custom styles for Container component.

--- packages/ljmv-theme/src/components/partials/container.js
+++ packages/ljmv-theme/src/components/partials/container.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {css} from 'frontity'
 
-const Container = ({children}) => (
-  <div css={css`
+const Container = ({children, className}) => (
+  <div className={className} css={css`
     max-width: 960px;
     padding: 0 1rem;
     margin: 0 auto; 
